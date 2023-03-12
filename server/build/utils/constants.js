cf2c3e48a85e687df52e95277f4f30995c5ed6a2
admin <admin@adminui-MacBookAir.local>
2023-03-12 17:08:21 +0900

    delete a build

--- server/build/utils/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.IMAGES_FOLDER_PATH = void 0;
-const IMAGES_FOLDER_PATH = '/public/images/';
-exports.IMAGES_FOLDER_PATH = IMAGES_FOLDER_PATH;
-//# sourceMappingURL=constants.js.map
